09a53e22d659daf157deadec3920feaa681fe952
Jozue06 <jozue06@gmail.com>
2018-07-03 01:28:09 -0700

    integrate

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Build Status](https://travis-ci.com/jozue06/16-basic-authentication.svg?branch=josh)](https://travis-ci.com/jozue06/16-basic-authentication)
+
+Heroku: https://josh-17.herokuapp.com/
+PR: https://github.com/jozue06/17-bearer-authorization/pull/1
+
+## 17-bearer-authentication
+
+
+
+**This lab used codefellows 17 demo code**
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 
 import authRouter from './auth/router.js';
+import publicRoute from './api/publicApi.js';
 
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/404.js';
@@ -17,6 +18,7 @@ app.use(express.json());  // => req.body
 app.use(express.urlencoded({extended:true})); // req.body => from a form's key value pairs
 
 app.use(authRouter);
+app.use(publicRoute);
 
 app.use(notFound);
 app.use(errorHandler);
