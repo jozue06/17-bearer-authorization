1341ea6a843c4bc9cde5ec64b80ef3a7e018a5df
Jozue06 <33652401+jozue06@users.noreply.github.com>
2018-09-05 15:59:48 -0700

    Update model.js

--- src/auth/model.js
+++ src/auth/model.js
@@ -7,6 +7,8 @@ import jwt from 'jsonwebtoken';
 const userSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
+  email: {type: String, required: true}
+  
 });
 
 // Before we save, hash the plain text password
